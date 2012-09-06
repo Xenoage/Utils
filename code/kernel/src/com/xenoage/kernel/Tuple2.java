d8f6176b98c27090ab149f766c0a1f6b2085ab00
Andreas Wenger <andi@xenoage.com>
2012-09-06 10:04:11 +0200

    Project kernel

---
+++ code/kernel/src/com/xenoage/kernel/Tuple2.java
@@ -0,0 +1,43 @@
+package com.xenoage.kernel;
+
+
+/**
+ * This class combines two instances of two given classes
+ * to one instance.
+ * 
+ * This is especially useful for multiple return values.
+ * 
+ * @author Andreas Wenger
+ */
+public final class Tuple2<T1, T2>
+{
+
+	private final T1 e1;
+	private final T2 e2;
+
+
+	public Tuple2(T1 e1, T2 e2)
+	{
+		this.e1 = e1;
+		this.e2 = e2;
+	}
+
+
+	public static <T1, T2> Tuple2<T1, T2> t(T1 e1, T2 e2)
+	{
+		return new Tuple2<T1, T2>(e1, e2);
+	}
+
+
+	public T1 get1()
+	{
+		return e1;
+	}
+
+
+	public T2 get2()
+	{
+		return e2;
+	}
+
+}
---
+++ code/kernel/src/com/xenoage/kernel/Tuple3.java
@@ -0,0 +1,51 @@
+package com.xenoage.kernel;
+
+
+/**
+ * This class combines three instances of three given classes
+ * to one instance.
+ * 
+ * This is especially useful for multiple return values.
+ * 
+ * @author Andreas Wenger
+ */
+public class Tuple3<T1, T2, T3>
+{
+
+	private final T1 e1;
+	private final T2 e2;
+	private final T3 e3;
+
+
+	public Tuple3(T1 e1, T2 e2, T3 e3)
+	{
+		this.e1 = e1;
+		this.e2 = e2;
+		this.e3 = e3;
+	}
+
+
+	public static <P1, P2, P3> Tuple3<P1, P2, P3> t3(P1 e1, P2 e2, P3 e3)
+	{
+		return new Tuple3<P1, P2, P3>(e1, e2, e3);
+	}
+
+
+	public T1 get1()
+	{
+		return e1;
+	}
+
+
+	public T2 get2()
+	{
+		return e2;
+	}
+
+
+	public T3 get3()
+	{
+		return e3;
+	}
+
+}
