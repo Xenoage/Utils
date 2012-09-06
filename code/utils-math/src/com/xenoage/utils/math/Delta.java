4ca9548f79862bb43f7dac5f366af9c76bb9ee14
Andreas Wenger <andi@xenoage.com>
2012-09-06 12:26:50 +0200

    utils-math extended

---
+++ code/utils-math/src/com/xenoage/utils/math/Delta.java
@@ -0,0 +1,31 @@
+package com.xenoage.utils.math;
+
+
+/**
+ * This class contains delta values (epsilon)
+ * for unit tests.
+ *
+ * @author Andreas Wenger
+ */
+public class Delta
+{
+  
+	/** Can be used to compare floats for equality. High precision. */
+  public static final float DELTA_FLOAT = 0.0000001f;
+  
+  /** Can be used to compare floats for equality. Low precision. */
+  public static final float DELTA_FLOAT_ROUGH = 0.001f;
+  
+  /** Abbreviation for {@link #DELTA_FLOAT} */
+  public static final float Df = DELTA_FLOAT;
+  
+  /** Abbreviation for {@link #DELTA_FLOAT_ROUGH} */
+  public static final float DRf = DELTA_FLOAT;
+  
+  
+  public static boolean equals(float v1, float v2)
+  {
+  	return Math.abs(v1 - v2) < DELTA_FLOAT;
+  }
+
+}
