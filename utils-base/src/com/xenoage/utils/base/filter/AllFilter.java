76c7ec258387c9523d01cef7f070c9a86531339e
Andreas Wenger <andi@xenoage.com>
2012-09-06 17:34:20 +0200

    Filters added

---
+++ utils-base/src/com/xenoage/utils/base/filter/AllFilter.java
@@ -0,0 +1,20 @@
+package com.xenoage.utils.base.filter;
+
+import java.util.List;
+
+
+/**
+ * Filter which selects all values.
+ * 
+ * @author Andreas Wenger
+ */
+public class AllFilter<T>
+	implements Filter<T>
+{
+	
+	@Override public List<T> filter(List<T> values)
+	{
+		return values;
+	}
+
+}
