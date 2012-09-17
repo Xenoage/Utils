462ee824d3d14abd4262b35fec8524c5fd981eca
Andreas Wenger <andi@xenoage.com>
2012-09-17 16:05:14 +0200

    URLUtils added

---
+++ utils-base/src/com/xenoage/utils/io/URLUtils.java
@@ -0,0 +1,28 @@
+package com.xenoage.utils.io;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+/**
+ * Some useful URL functions.
+ *
+ * @author Andreas Wenger
+ */
+public class URLUtils
+{
+
+	/**
+	 * Returns true, if the given String contains an absolute URL.
+	 */
+	public static boolean isAbsoluteURL(String path)
+	{
+		try {
+			new URL(path);
+			return true;
+		} catch (MalformedURLException e) {
+			return false;
+		}
+	}
+
+}
