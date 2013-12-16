3e5571a294e8a919558cedc2e9cb35764240f27f
Andreas Wenger <andi@xenoage.com>
2013-12-16 20:37:49 +0100

    NullableList for all types of List

--- utils-base/src/com/xenoage/utils/collections/NullableList.java
+++ utils-base/src/com/xenoage/utils/collections/NullableList.java
@@ -5,17 +5,19 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Helper class to save memory when using {@link ArrayList}s.
+ * Helper class to save memory when using {@link List}s.
  * 
  * When the list is empty, it is null. As soon as it has at least one
  * element, it is not null any more.
  * 
- * It can be used to modify nullable {@link ArrayList}s in a comforable way
+ * It can be used to modify nullable {@link List}s in a comforable way
  * without null checks, e.g.:
  * <code>object.setElements(NullableList.add(object.getElements(), newElement);</code>.
  * 
- * For looping over the list, use {@link #it(ArrayList)} for getting an {@link Iterable}
- * or {@link #it(ArrayList)} and {@link #get(ArrayList, int)}.
+ * For looping over the list, use {@link #it(List)} for getting an {@link Iterable}
+ * or {@link #it(List)} and {@link #get(List, int)}.
+ * 
+ * When a new list is created, the {@link ArrayList} class is used.
  * 
  * @author Andreas Wenger
  */
@@ -29,7 +31,7 @@ public class NullableList {
 	 * If the list was null, it is instantiated now.
 	 * The modified list is returned.
 	 */
-	public static <T> ArrayList<T> add(ArrayList<T> list, T e) {
+	public static <T> List<T> add(List<T> list, T e) {
 		if (list == null)
 			list = new ArrayList<T>();
 		list.add(e);
@@ -41,7 +43,7 @@ public class NullableList {
 	 * If the list is empty now, null is returned, otherwise
 	 * the modified list is returned.
 	 */
-	public static <T> ArrayList<T> remove(ArrayList<T> list, T e) {
+	public static <T> List<T> remove(List<T> list, T e) {
 		if (list == null)
 			return null;
 		list.remove(e);
@@ -55,7 +57,7 @@ public class NullableList {
 	 * If the list is empty now, null is returned, otherwise
 	 * the modified list is returned.
 	 */
-	public static <T> ArrayList<T> remove(ArrayList<T> list, int index) {
+	public static <T> List<T> remove(List<T> list, int index) {
 		if (list == null)
 			return null;
 		list.remove(index);
@@ -67,7 +69,7 @@ public class NullableList {
 	/**
 	 * Gets an iterable over the given list, even when it is null.
 	 */
-	@SuppressWarnings("unchecked") public static <T> Iterable<T> it(ArrayList<T> list) {
+	@SuppressWarnings("unchecked") public static <T> Iterable<T> it(List<T> list) {
 		if (list == null)
 			return (List<T>) emptyList; //this works, see Collections.emptyList()
 		return list;
@@ -76,7 +78,7 @@ public class NullableList {
 	/**
 	 * Gets the size of the given list, or 0 when it is null.
 	 */
-	public static <T> int size(ArrayList<T> list) {
+	public static <T> int size(List<T> list) {
 		if (list == null)
 			return 0;
 		return list.size();
@@ -86,7 +88,7 @@ public class NullableList {
 	 * Gets the element at the given index, or throws an {@link IndexOutOfBoundsException}
 	 * if the index is out of bounds or the list is null.
 	 */
-	public static <T> T get(ArrayList<T> list, int index) {
+	public static <T> T get(List<T> list, int index) {
 		if (list == null)
 			throw new IndexOutOfBoundsException();
 		return list.get(index);
