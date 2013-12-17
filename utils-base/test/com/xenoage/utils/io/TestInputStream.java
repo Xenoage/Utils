c9725cc61000c0733fc33fb256eca505bfa2c8f3
Andreas Wenger <andi@xenoage.com>
2013-12-17 23:33:31 +0100

    BufferedInputStream

---
+++ utils-base/test/com/xenoage/utils/io/TestInputStream.java
@@ -0,0 +1,32 @@
+package com.xenoage.utils.io;
+
+import java.io.IOException;
+
+/**
+ * Stream which returns the numbers from 0 to (length - 1) % 256.
+ * 
+ * @author Andreas Wenger
+ */
+public class TestInputStream
+	implements InputStream {
+	
+	private int length;
+	private int pos = 0;
+	
+	public TestInputStream(int length) {
+		this.length = length;
+	}
+
+	@Override public int read()
+		throws IOException {
+		if (pos >= length)
+			return -1;
+		int ret = pos % 256;
+		pos++;
+		return ret;
+	}
+
+	@Override public void close() {
+	}
+
+}
