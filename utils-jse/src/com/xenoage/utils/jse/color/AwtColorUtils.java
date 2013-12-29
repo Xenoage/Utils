b195e02d304a7ae7fa68690309c50efee04d053b
Andreas Wenger <andi@xenoage.com>
2013-12-29 23:59:33 +0100

    Renamed methods in AwtColorUtils

--- utils-jse/src/com/xenoage/utils/jse/color/AwtColorUtils.java
+++ utils-jse/src/com/xenoage/utils/jse/color/AwtColorUtils.java
@@ -14,14 +14,14 @@ public class AwtColorUtils {
 	 * Gets the {@link Color} from the given AWT color.
 	 * @param awtColor  the AWT color
 	 */
-	public static Color fromAWTColor(java.awt.Color awtColor) {
+	public static Color fromAwtColor(java.awt.Color awtColor) {
 		return new Color(awtColor.getRed(), awtColor.getGreen(), awtColor.getBlue(), awtColor.getAlpha());
 	}
 
 	/**
 	 * Gets the AWT color from the given {@link Color}.
 	 */
-	public static java.awt.Color toAWTColor(Color color) {
+	public static java.awt.Color toAwtColor(Color color) {
 		return new java.awt.Color(color.r, color.g, color.b, color.a);
 	}
 
