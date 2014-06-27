a9ec7c75650ac4c82595cbf29d84d06c8b20ebf4
Andreas Wenger <andi@xenoage.com>
2014-06-27 09:45:40 +0200

    Added readToString to JseStreamUtils, reused it for JseFileUtils

--- utils-jse/src/com/xenoage/utils/jse/io/JseStreamUtils.java
+++ utils-jse/src/com/xenoage/utils/jse/io/JseStreamUtils.java
@@ -1,9 +1,11 @@
 package com.xenoage.utils.jse.io;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Useful methods to work with streams.
@@ -32,6 +34,28 @@ public class JseStreamUtils {
 		while ((readBytes = in.read(bytes)) > 0)
 			outputStream.write(bytes, 0, readBytes);
 		return outputStream.toByteArray();
+	}
+	
+	/**
+	 * Reads the given input stream into a string and returns it.
+	 * If the file does not exist or there is another I/O error,
+	 * null is returned.
+	 */
+	public static String readToString(InputStream in) {
+		try {
+			StringBuffer fileData = new StringBuffer(1024);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			char[] buf = new char[1024];
+			int numRead = 0;
+			while ((numRead = reader.read(buf)) != -1) {
+				String readData = String.valueOf(buf, 0, numRead);
+				fileData.append(readData);
+			}
+			reader.close();
+			return fileData.toString();
+		} catch (IOException ex) {
+			return null;
+		}
 	}
 
 }
