7375fc472e4f18840a4e27ce9bdf010e9e10a06c
Andreas Wenger <andi@xenoage.com>
2014-11-03 08:20:38 +0100

    Removed unused import in LangResourceBundle

--- utils-jse/src/com/xenoage/utils/jse/lang/LangResourceBundle.java
+++ utils-jse/src/com/xenoage/utils/jse/lang/LangResourceBundle.java
@@ -3,7 +3,6 @@ package com.xenoage.utils.jse.lang;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
