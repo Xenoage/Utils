185f2cb1cfd436811460bfba4c0996e39266cdd9
Andreas Wenger <andi@xenoage.com>
2017-01-28 16:43:20 +0100

    Bug in BiMap fixed; TriMap added

--- utils-base/src/com/xenoage/utils/collections/BiMap.java
+++ utils-base/src/com/xenoage/utils/collections/BiMap.java
@@ -1,11 +1,14 @@
 package com.xenoage.utils.collections;
 
-import static com.xenoage.utils.collections.CollectionUtils.map;
+import com.xenoage.utils.annotations.MaybeNull;
+import com.xenoage.utils.annotations.NonNull;
+import com.xenoage.utils.kernel.Tuple2;
 
 import java.util.Map;
 
-import com.xenoage.utils.annotations.MaybeNull;
-import com.xenoage.utils.annotations.NonNull;
+import static com.xenoage.utils.CheckUtils.checkArgsNotNull;
+import static com.xenoage.utils.collections.CollectionUtils.map;
+import static com.xenoage.utils.kernel.Tuple2.t;
 
 /**
  * A bidirectional map, with constant lookup time in both directions.
@@ -14,16 +17,15 @@ import com.xenoage.utils.annotations.NonNull;
  * @author Andreas Wenger
  */
 public class BiMap<T1, T2> {
-	
-	private Map<T1, T2> map1 = map();
-	private Map<T2, T1> map2 = map();
-	private T1 defaultValue1 = null;
-	private T2 defaultValue2 = null;
-	
+
+	private Map<T1, Tuple2<T1, T2>> map1 = map();
+	private Map<T2, Tuple2<T1, T2>> map2 = map();
+	private Tuple2<T1, T2> defaultValue = t(null, null);
+
 	public static <T1, T2> BiMap<T1, T2> biMap() {
 		return new BiMap<T1, T2>();
 	}
-	
+
 	public int size() {
 		return map1.size();
 	}
@@ -31,50 +33,47 @@ public class BiMap<T1, T2> {
 	public boolean isEmpty() {
 		return map1.isEmpty();
 	}
-	
-	@MaybeNull public T1 get1(@MaybeNull T2 value2) {
-		if (value2 == null)
-			return defaultValue1;
-		T1 ret = map2.get(value2);
-		return (ret != null ? ret : defaultValue1);
+
+	@MaybeNull public T2 getBy1(@MaybeNull T1 value1) {
+		if (value1 == null)
+			return defaultValue.get2();
+		Tuple2<T1, T2> ret = map1.get(value1);
+		return (ret != null ? ret : defaultValue).get2();
 	}
 
-	@MaybeNull public T2 get2(@MaybeNull T1 value1) {
-		if (value1 == null)
-			return defaultValue2;
-		T2 ret = map1.get(value1);
-		return (ret != null ? ret : defaultValue2);
+	@MaybeNull public T1 getBy2(@MaybeNull T2 value2) {
+		if (value2 == null)
+			return defaultValue.get1();
+		Tuple2<T1, T2> ret = map2.get(value2);
+		return (ret != null ? ret : defaultValue).get1();
 	}
 
 	public void put(@NonNull T1 value1, @NonNull T2 value2) {
+		checkArgsNotNull(value1, value2);
 		removeOldValues(value1, value2);
-		map1.put(value1, value2);
-		map2.put(value2, value1);
+		Tuple2<T1, T2> value = t(value1, value2);
+		map1.put(value1, value);
+		map2.put(value2, value);
 	}
 
 	private void removeOldValues(T1 value1, T2 value2) {
-		T1 oldValue1 = map2.get(value2);
-		if (oldValue1 != null)
-			map1.remove(oldValue1);
-		T2 oldValue2 = map1.get(value1);
-		if (oldValue2 != null)
-			map2.remove(oldValue2);
-	}
-	
-	public T1 getDefaultValue1() {
-		return defaultValue1;
+		remove(map1.get(value1));
+		remove(map2.get(value2));
 	}
 
-	public void setDefaultValue1(T1 defaultValue1) {
-		this.defaultValue1 = defaultValue1;
+	private void remove(Tuple2<T1, T2> value) {
+		if (value != null) {
+			map1.remove(value.get1());
+			map2.remove(value.get2());
+		}
 	}
 
-	public T2 getDefaultValue2() {
-		return defaultValue2;
+	public Tuple2<T1, T2> getDefaultValue() {
+		return defaultValue;
 	}
 
-	public void setDefaultValue2(T2 defaultValue2) {
-		this.defaultValue2 = defaultValue2;
+	public void setDefaultValue(T1 defaultValue1, T2 defaultValue2) {
+		this.defaultValue = t(defaultValue1, defaultValue2);
 	}
 
 }
--- utils-test/test/com/xenoage/utils/collections/BiMapTest.java
+++ utils-test/test/com/xenoage/utils/collections/BiMapTest.java
@@ -1,9 +1,9 @@
 package com.xenoage.utils.collections;
 
-import static com.xenoage.utils.collections.BiMap.biMap;
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static com.xenoage.utils.collections.BiMap.biMap;
+import static org.junit.Assert.*;
 
 /**
  * Tests for {@link BiMap}.
@@ -20,10 +20,10 @@ public class BiMapTest {
 		map.put("other", 8);
 		map.put("new", 5);
 		assertEquals(2, map.size());
-		assertEquals("new", map.get1(5));
-		assertEquals("other", map.get1(8));
-		assertEquals((Integer) 5, map.get2("new"));
-		assertEquals((Integer) 8, map.get2("other"));
+		assertEquals("new", map.getBy2(5));
+		assertEquals("other", map.getBy2(8));
+		assertEquals((Integer) 5, map.getBy1("new"));
+		assertEquals((Integer) 8, map.getBy1("other"));
 	}
 	
 	@Test public void testSameTypes() {
@@ -33,10 +33,22 @@ public class BiMapTest {
 		map.put("2", "1");
 		map.put("1", "3");
 		assertEquals(2, map.size());
-		assertEquals("2", map.get1("1"));
-		assertEquals("1", map.get2("2"));
-		assertEquals("1", map.get1("3"));
-		assertEquals("3", map.get2("1"));
+		assertEquals("2", map.getBy2("1"));
+		assertEquals("1", map.getBy1("2"));
+		assertEquals("1", map.getBy2("3"));
+		assertEquals("3", map.getBy1("1"));
+	}
+
+	@Test public void testDeadReferences() {
+		BiMap<String, Integer> map = biMap();
+		map.put("A", 1);
+		map.put("B", 2);
+		//now, when 2 is overwritten, the reference of B must be dead
+		map.put("C", 2);
+		assertEquals(null, map.getBy1("B"));
+		//and when A is overwritten, the reference to 1 must be dead
+		map.put("A", 3);
+		assertEquals(null, map.getBy2(3));
 	}
 
 }
