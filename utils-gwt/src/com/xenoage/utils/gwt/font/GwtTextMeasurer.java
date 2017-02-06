ac6031db91df3b307542439e3295dbaf608b8c5e
Andreas Wenger <andi@xenoage.com>
2017-02-06 18:43:47 +0100

    webapp: font rendering improved

--- utils-gwt/src/com/xenoage/utils/gwt/font/GwtTextMeasurer.java
+++ utils-gwt/src/com/xenoage/utils/gwt/font/GwtTextMeasurer.java
@@ -1,20 +1,39 @@
 package com.xenoage.utils.gwt.font;
 
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.dom.client.CanvasElement;
+import com.google.gwt.dom.client.Document;
 import com.xenoage.utils.font.FontInfo;
 import com.xenoage.utils.font.TextMeasurer;
 import com.xenoage.utils.font.TextMetrics;
+import com.xenoage.utils.math.Units;
 
 /**
  * GWT implementation of a {@link TextMeasurer}.
- * Currently just a dummy, which returns always 0.
- * 
+ *
  * @author Andreas Wenger
  */
 public class GwtTextMeasurer
-	implements TextMeasurer {
+		implements TextMeasurer {
+
+	private static CanvasElement canvas = null;
 
 	@Override public TextMetrics measure(FontInfo font, String text) {
-		return new TextMetrics(0, 0, 0, 0); //TODO
+		Context2d context = getContext();
+		context.setFont(GwtFontUtils.getCssFont(font));
+		com.google.gwt.canvas.dom.client.TextMetrics metrics = context.measureText(text);
+		float width = Units.pxToMm((float) metrics.getWidth(), 1);
+		//we do not know the ascent from HTML. we use the width of "W" instead, which should be similar.
+		//as the descent and the leading we usethe third of the ascent.n ha
+		metrics = context.measureText("W");
+		float ascent = Units.pxToMm((float) metrics.getWidth(), 1);
+		return new TextMetrics(ascent, ascent / 3, ascent / 3, width);
+	}
+
+	private static Context2d getContext() {
+		if (canvas == null)
+			canvas = (CanvasElement) Document.get().createElement("canvas");
+		return canvas.getContext2d();
 	}
 
 }
